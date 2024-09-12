a8d6425bf85452402dd785d96bd7011985e1d4dc
Badr Eddine El Oirghi <bel-oirg@e3r11p2.1337.ma>
2024-09-12 12:30:25 +0100

    new one

--- Makefile
+++ Makefile
@@ -1,78 +1,30 @@
 CC = cc
-INC_DIR = inc
-SRC_DIR = src
-LIB_DIR = libs
-OBJ_DIR = obj
-BIN_DIR = bin
-MAPS_DIR = maps
-EXE = cub3d
+NAME = cub3D  
+GLFLAGS = -ffast-math -framework Cocoa -framework OpenGL -framework IOKit -lglfw# -fsanitize=address 
+# CFLAGS =  
+RM = rm -rf
 
-MLX_INC = $(LIB_DIR)/mlx/inc
-FTLIB_INC = $(LIB_DIR)/ftlib/inc
-ifeq ($(UNAME_S), Darwin)
-GLFW_LIB = -L /Users/$(USER)/brew/lib/libglfw3.a
-endif 
+SRCS = cub_raw.c main.c move_player.c wall_render.c
 
-MLX_LIB = $(LIB_DIR)/mlx/build/libmlx42.a
-FT_LIB = $(LIB_DIR)/ftlib/bin/ftlib.a
-FRAMEWORK = -O3 -ffast-math -framework Cocoa -framework OpenGL -framework IOKit -lglfw
-ifeq ($(UNAME_S), Darwin)
-    GLFW_INCLUDE = -I /Users/$(USER)/brew/Cellar/glfw/3.4/include/GLFW/glfw3.h
-endif
+OBJS = $(SRCS:.c=.o)
+HEADER = cub.h
 
-LIBS = $(FRAMEWORK) $(FT_LIB) $(MLX_LIB) $(GLFW_LIB)
-INCS = -I $(INC_DIR) -I $(FTLIB_INC) -I $(MLX_INC) $(GLFW_INCLUDE)
-SRCS = $(shell find $(SRC_DIR) -name '*.c')
-OBJS = $(patsubst $(SRC_DIR)/%, $(OBJ_DIR)/%, $(SRCS:.c=.o))
-NAME = $(BIN_DIR)/$(EXE)
+all : $(NAME)
 
-ifeq ($(MAKECMDGOALS),debug)
-CFLAGS = -Wall -Wextra -Werror -fsanitize=address -g -pthread
-BUILDING_TYPE = debug
-else
-CFLAGS = -Wall -Wextra -Werror -fsanitize=address -g -pthread
-BUILDING_TYPE = release
-endif
+$(NAME) : $(OBJS) 
+	$(CC) $(GLFLAGS) $(OBJS) -o $(NAME)
 
-all: build $(NAME)
-# all: $(NAME)
-	@mkdir -p $(OBJ_DIR) $(BIN_DIR)
-	@echo "-- Building executable done"
-	@echo "-- Binary executable have been written to: $(PWD)/$(BIN_DIR)/$(EXE)"
-ifeq ($(BUILDING_TYPE), release)
-	@echo "-- Building in RELEASE mode"
-endif
+%.o : %.c
+	$(CC) $(GLFLAGS) -c $< -o $@
 
-build:
-	@mkdir -p $(OBJ_DIR) $(BIN_DIR)
-ifeq ($(BUILDING_TYPE), debug)
-	@cmake -DDEBUG=1 $(LIB_DIR)/mlx -B $(LIB_DIR)/mlx/build && make -C $(LIB_DIR)/mlx/build -j4
-	@make debug -C $(LIB_DIR)/ftlib
-else
-	@cmake $(LIB_DIR)/mlx -B $(LIB_DIR)/mlx/build && make -C $(LIB_DIR)/mlx/build -j4
-	@make -C $(LIB_DIR)/ftlib
-endif
+$(OBJS) : $(HEADER)
 
-$(OBJ_DIR)/%.o : $(SRC_DIR)/%.c $(INC_DIR)/*.h
-	@mkdir -p $(@D)
-	@$(CC) $(CFLAGS) -I$(INC_DIR) -c $< -o $@
+fclean : clean
+	$(RM) $(NAME)
 
-$(NAME): $(OBJS)
-	@echo "-- Building executable : $@"
-	@$(CC) $(OBJS) $(LIBS) -g -fsanitize=address -o $(NAME)
+clean :
+	$(RM) $(OBJS)
 
-debug: all
-	@echo "-- Building in DEBUG mode"
+re : fclean all
 
-clean:
-	@rm -rf $(OBJ_DIR)
-	@make clean -C $(LIB_DIR)/ftlib
-
-fclean:
-	@rm -rf $(BIN_DIR)
-	@rm -rf $(LIB_DIR)/mlx/build
-	@make fclean -C $(LIB_DIR)/ftlib
-
-re: fclean all
-
-.PHONY: all, clean, fclean, re, build, debug,
+.PHONY: all clean fclean re
---
+++ cub_raw.c
@@ -0,0 +1,132 @@
+// example:
+// cc -O3 -ffast-math -framework Cocoa -framework OpenGL -framework IOKit -lglfw (path to libmlx42.a) -L(path to glfw library) cub3d.c -o cub
+
+#include "cub.h"
+
+int	unit_circle(float angle, char c)	// check the unit circle
+{
+	if (c == 'x')
+	{
+		if (angle > 0 && angle < M_PI)
+			return (1);
+	}
+	else if (c == 'y')
+	{
+		if (angle > (M_PI / 2) && angle < (3 * M_PI) / 2)
+			return (1);
+	}
+	return (0);
+}
+
+int	inter_check(float angle, float *inter, float *step, int is_horizon)	// check the intersection
+{
+	if (is_horizon)
+	{
+		if (angle > 0 && angle < M_PI)
+		{
+			*inter += TILE_SIZE;
+			return (-1);
+		}
+		*step *= -1;
+	}
+	else
+	{
+		if (!(angle > M_PI / 2 && angle < 3 * M_PI / 2)) 
+		{
+			*inter += TILE_SIZE;
+			return (-1);
+		}
+		*step *= -1;
+	}
+	return (1);
+}
+
+int	wall_hit(float x, float y, t_mlx *mlx)	// check the wall hit
+{
+	int		x_m;
+	int		y_m;
+
+	if (x < 0 || y < 0)
+		return (0);
+	x_m = floor (x / TILE_SIZE); // get the x position in the map
+	y_m = floor (y / TILE_SIZE); // get the y position in the map
+	if ((y_m >= mlx->dt->h_map || x_m >= mlx->dt->w_map))
+		return (0);
+	if (mlx->dt->map2d[y_m] && x_m <= (int)strlen(mlx->dt->map2d[y_m]))
+		if (mlx->dt->map2d[y_m][x_m] == '1')
+			return (0);
+	return (1);
+}
+
+float	get_h_inter(t_mlx *mlx, float angl)	// get the horizontal intersection
+{
+	float	h_x;
+	float	h_y;
+	float	x_step;
+	float	y_step;
+	int		pixel;
+
+	y_step = TILE_SIZE;
+	x_step = TILE_SIZE / tan(angl);
+	h_y = floor(mlx->ply->plyr_y / TILE_SIZE) * TILE_SIZE;
+	pixel = inter_check(angl, &h_y, &y_step, 1);
+	h_x = mlx->ply->plyr_x + (h_y - mlx->ply->plyr_y) / tan(angl);
+	if ((unit_circle(angl, 'y') && x_step > 0) || (!unit_circle(angl, 'y') && x_step < 0)) // check x_step value
+		x_step *= -1;
+	while (wall_hit(h_x, h_y - pixel, mlx)) // check the wall hit whit the pixel value
+	{
+		h_x += x_step;
+		h_y += y_step;
+	}
+	return (sqrt(pow(h_x - mlx->ply->plyr_x, 2) + pow(h_y - mlx->ply->plyr_y, 2))); // get the distance
+}
+
+float	get_v_inter(t_mlx *mlx, float angl)	// get the vertical intersection
+{
+	float	v_x;
+	float	v_y;
+	float	x_step;
+	float	y_step;
+	int		pixel;
+
+	x_step = TILE_SIZE; 
+	y_step = TILE_SIZE * tan(angl);
+    
+	v_x = floor(mlx->ply->plyr_x / TILE_SIZE) * TILE_SIZE;
+	pixel = inter_check(angl, &v_x, &x_step, 0); // check the intersection and get the pixel value
+	v_y = mlx->ply->plyr_y + (v_x - mlx->ply->plyr_x) * tan(angl);
+	if ((unit_circle(angl, 'x') && y_step < 0) || (!unit_circle(angl, 'x') && y_step > 0)) // check y_step value
+		y_step *= -1;
+	while (wall_hit(v_x - pixel, v_y, mlx)) // check the wall hit whit the pixel value
+	{
+		v_x += x_step;
+		v_y += y_step;
+	}
+	return (sqrt(pow(v_x - mlx->ply->plyr_x, 2) + pow(v_y - mlx->ply->plyr_y, 2))); // get the distance
+}
+
+void	cast_rays(t_mlx *mlx)	
+{
+	double	h_inter;
+	double	v_inter;
+	int		ray;
+
+	ray = 0;
+	mlx->ray->ray_ngl = mlx->ply->angle - (mlx->ply->fov_rd / 2); // the start angle
+	while (ray < S_W) 
+	{
+		mlx->ray->flag = 0; // flag for the wall
+		h_inter = get_h_inter(mlx, nor_angle(mlx->ray->ray_ngl)); // get the horizontal intersection
+		v_inter = get_v_inter(mlx, nor_angle(mlx->ray->ray_ngl)); // get the vertical intersection
+		if (v_inter <= h_inter) // check the distance
+			mlx->ray->distance = v_inter; // get the distance
+		else
+		{
+			mlx->ray->distance = h_inter; // get the distance
+			mlx->ray->flag = 1; // flag for the wall
+		}
+		render_wall(mlx, ray); // render the wall
+		ray++; // next ray
+		mlx->ray->ray_ngl += (mlx->ply->fov_rd / S_W); // next angle
+	}
+}
